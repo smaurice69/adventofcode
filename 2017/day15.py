fd17a3a1c581ef139f5382bfdde5a6f6caa1c039
Stein Erik Maurice <steinerik.maurice@hotmail.com>
2025-11-12 13:59:44 +0100

    Ferdig med 15b

--- 2017/day15.py
+++ 2017/day15.py
@@ -6,37 +6,68 @@ import sys
 import re
 from collections import deque
 
+
+
 ROOT = Path(__file__).resolve().parents[1]
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
 
 from utils.file_parsers import read_lines
 
+def partA(genA:int, genB:int, factorA:int, factorB:int) -> int:
+
+    count = 0
+    for i in range(40000000):
+        nextvalA = (genA * factorA) % 2147483647
+        nextvalB = (genB * factorB) % 2147483647
+        genA = nextvalA
+        genB = nextvalB
+        # print("A = ", genA, "  B = ", genB)
+        if genA & 0xFFFF == genB & 0xFFFF:
+            count += 1
+
+    return count
+
+
+def partB(genA:int, genB:int, factorA:int, factorB:int) -> int:
+    count = 0
+#    nextvalA = (genA * factorA) % 2147483647
+#    nextvalB = (genB * factorB) % 2147483647
+
+    for i in range(5000000):
+        #while nextvalA % 4 != 0:
+        while True:
+            genA = (genA * factorA) % 2147483647
+            if genA % 4 == 0:
+                break
+
+        while True:
+            genB = (genB * factorB) % 2147483647
+            if genB % 8 == 0:
+                break
+
+        if(genA & 0xFFFF == genB & 0xFFFF):
+            count += 1
+    #        print("i = ", i, " A = ", genA, "  B = ", genB)
+
+
+
+    return count
 
 
 def main():
    # lines = read_lines(Path(__file__).resolve().parent / 'input/day13.txt')
 
-    genA = 591 #65
-    genB = 393 #8921
+    genA = 591
+    genB = 393
     factorA = 16807
     factorB = 48271
-    count = 0
-
-    for i in range(40000000):
-        nextvalA = (genA * factorA) % 2147483647
-        nextvalB = (genB * factorB ) % 2147483647
-        genA = nextvalA
-        genB = nextvalB
-        #print("A = ", genA, "  B = ", genB)
-        if genA & 0xFFFF == genB & 0xFFFF:
-        #    print("Match, i =", i)
-            count += 1
 
 
-    print("Day 15 a = ", count)
 
-    print("Day 15 b = ", 0)
+    print("Day 15 a = ", partA(genA, genB, factorA, factorB))
+
+    print("Day 15 b = ", partB(genA, genB, factorA, factorB))
 
     
 
